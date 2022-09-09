d4084a940311ebfa60134a7322db2f214ce24f0f
franco <kws60000@naver.com>
2022-09-09 11:07:52 +0900

    Link, metadata

---
+++ pages/posts/first-post.js
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import Head from "next/head";
+import Layout from "../components/Layout";
+
+export default function FirstPost() {
+  return (
+    <Layout>
+      <Head>
+        <title>첫 번째 포스트</title>
+      </Head>
+      <h1>첫 포스트!</h1>
+      <h2>
+        <Link href="/">
+          <a>홈으로 돌아가기</a>
+        </Link>
+      </h2>
+    </Layout>
+  )
+}
